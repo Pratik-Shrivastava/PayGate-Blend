5d9034cebb68cd78fb85e47a6f8b04d272aabd0f
Pratik Shrivastava <pratikkumar441@gmail.com>
2025-04-23 17:29:53 +0530

    feat: add the main method for PayGateApp

---
+++ src/main/java/com/pratik/payGateBlend/PayGateApp.java
@@ -0,0 +1,13 @@
+package com.pratik.payGateBlend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PayGateApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PayGateApp.class, args);
+	}
+}
