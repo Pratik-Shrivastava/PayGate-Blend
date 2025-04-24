11d8859b540d93c0c127f31f34eabd756380b2af
Pratik Shrivastava <pratikkumar441@gmail.com>
2025-04-24 09:47:32 +0530

    feat: add GeneralResponse java model

---
+++ src/main/java/com/pratik/payGateBlend/common/model/GeneralResponse.java
@@ -0,0 +1,18 @@
+package com.pratik.payGateBlend.common.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GeneralResponse {
+	
+	boolean status;
+	int status_code;
+	String message;
+	Object data;
+}
